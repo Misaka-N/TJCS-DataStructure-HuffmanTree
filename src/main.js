054e336234349b69456a7362b72372ecc683b39d
Misaka-N <122147034+Misaka-N@users.noreply.github.com>
2023-08-11 19:56:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import App from '@/components/App.vue'
+import App from './components/App.vue'
 
 const app = createApp(App)
 app.use(ElementPlus)
